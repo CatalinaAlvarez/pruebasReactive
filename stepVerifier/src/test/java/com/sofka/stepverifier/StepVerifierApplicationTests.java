14ad07493aabbdc6b6d30905e6da2d90d343c42a
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2021-12-08 19:17:17 -0500

    Se agregan los archivos de stepverifier con los 3 primeros test

---
+++ stepVerifier/src/test/java/com/sofka/stepverifier/StepVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.stepverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepVerifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
